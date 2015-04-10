52e0f6302cfa5762cc0cce7a8a13d686c21c129e
Simon Rouchier <s.rouchier@gmail.com>
2015-04-10 11:13:34 +0200

    Restructured the entire repository

--- hygrothermal/passys/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 06 11:03:00 2014
-
-@author: Rouchier
-"""
-
---
+++ wall/thermal/images/some_graphs.py
@@ -0,0 +1,18 @@
+
+"""
+Quelques graphes qui apparaissent sur le wordpress mais pas dans les notebooks
+"""
+
+ax = plt.subplot(121)
+
+plt.plot(time_file, u_file, )
+
+plt.hist(lambda_1_samples, histtype='stepfilled', bins=30, alpha=0.85,
+         label="posterior of $\lambda_1$", color="#A60628", normed=True)
+
+
+plt.legend(loc="upper left")
+plt.title(r"""Posterior distributions of the variables
+    $\lambda_1,\;\lambda_2,\;\tau$""")
+plt.xlim([15, 30])
+plt.xlabel("$\lambda_1$ value")
