7a3610c5cc23de38583d1081941a18058c6587a0
srouchier <s.rouchier@gmail.com>
2021-08-17 15:22:29 +0200

    All theoretical parts are done, most tutorials still missing

--- build_the_book.R
+++
@@ -1,7 +0,0 @@
-library(bookdown)
-
-render_book(input = ".", output_format = NULL, clean = TRUE,
-            envir = parent.frame(), clean_envir = !interactive(),
-            output_dir = NULL, new_session = NA, preview = FALSE,
-            config_file = "_bookdown.yml")
-
